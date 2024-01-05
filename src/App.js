e24edb0fd07523fb3d758b4d635bfb4d7edd94cf
nishabisht <40986426+nishabisht@users.noreply.github.com>
2024-01-05 16:56:13 +0530

    React BMI caculator project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <form className="bmiForm">
+          <h4 className="bmiHeading">BMI Calculator</h4>
+          <div className="mb-3">
+            <label className="form-label" id="p1" for="weight">
+              Weight(KG)
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="weight"
+              aria-describedby="weight"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" id="p1" for="height">
+              Height(Feet)
+            </label>
+            <input
+              type="number"
+              min="1"
+              max="9"
+              id="height"
+              className="form-control"
+              aria-describedby="height"
+            />
+            <div id="heightHelp" className="form-text">
+              Minimum height 1 feet & maximum height 9 feet
+            </div>
+          </div>
+          <div className="">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <div className="">
+            <button type="reset" className="btn btn-light">
+              Reload
+            </button>
+          </div>
+          <div>
+            <p className="bmiHeading" id="p1">
+              Your BMI is:
+            </p>
+          </div>
+        </form>
+      }
     </div>
   );
 }
